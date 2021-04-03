e8e4f05f00630871d66bb11e7063c9bf1f05717f
Logan Mortimer <logan@isthisit.nz>
2021-04-04 10:50:07 +1200

    CrosswordList component

--- src/View/CrosswordList.fs
+++ src/View/CrosswordList.fs
@@ -1,7 +1,45 @@
 module View.CrosswordList
 
 open Feliz
+open Fetch
+open Fable.Core
+open Fable.SimpleJson
+
+// decodes public/puzzles.json
+type private PuzzleList = string list
+
+type State = {
+    crosswords: string list
+}
+
+type Msg = 
+    | CrosswordListLoaded of string list
+
+let update (state: State) = function
+    | CrosswordListLoaded (crosswords) -> { state with crosswords = crosswords }
 
 [<ReactComponent>]
 let CrosswordList() =
-    Html.h1 "List of Crosswords"
+
+    let (state, dispatch) = React.useReducer(update, { crosswords = [] })
+
+    let loadData() = async {
+
+        let! response = fetch "puzzles.json" [] |> Async.AwaitPromise
+        let! data = response.text() |> Async.AwaitPromise
+
+        match Json.tryParseAs<PuzzleList> data with
+        | Ok v -> dispatch(CrosswordListLoaded(v))
+        | Error err -> JS.console.error(err)
+    }
+
+    let crosswords = 
+        state.crosswords 
+        |> List.map (fun v -> Html.li v)
+
+    React.useEffect(loadData >> Async.StartImmediate, [| |])
+
+    Html.div [
+        Html.h1 "Crosswords"
+        Html.ul crosswords
+    ]
--- src/View/Page/Home.fs
+++ src/View/Page/Home.fs
@@ -1,7 +1,13 @@
 module View.Page.Home
 
 open Feliz
+open View.CrosswordList
 
 [<ReactComponent>]
 let HomePage() = 
-    Html.h1 "Home"
+    Html.div [
+        prop.children [
+            Html.h1 "Home"
+            CrosswordList()
+        ]
+    ]
