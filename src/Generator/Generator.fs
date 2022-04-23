29eaa383c1ebf7dd084634e510c671901ca65fc9
Logan Mortimer <logan@isthisit.nz>
2022-04-23 19:10:16 +1000

    Place a single letter

--- src/Generator/Generator.fs
+++ src/Generator/Generator.fs
@@ -1,5 +1,60 @@
 module Generator.Generator
 
-let generatePuzzle (dimension: int): int = 
+open Domain.Grid
 
-    1
+/// initialises a grid of height and width dimension filled with Black cells
+let makeEmptyGrid (dimension: int): Grid = 
+
+    [ for _ in 1 .. dimension -> [ for _ in 1 .. dimension -> Cell.Black ] ]
+    
+// index of the row, index of the column for the starting letter
+type Coord = int * int * Direction
+let findLocationsForWord (word: string) (grid: Grid): Result<Coord, string> =
+    
+    let wordLength = word.Length
+    
+    let firstRow = grid |> List.head
+    
+    let firstCoordForRow (word: string) (row: Cell list) (rowIndex: int): Result<Coord, string> =
+        let windows =
+            row
+            |> List.indexed
+            |> List.windowed word.Length
+            |> List.map (fun window ->
+                let windowStartingIndex = window |> List.head |> fst
+
+                // does the word fit the window
+                let letterCheckInWindow =
+                    window
+                    |> List.map (fun v ->
+                        let cellIndex = fst v
+                        let cell = snd v
+                        
+                        match cell with
+                        | Black -> true
+                        | _ -> false // else white and character matches is true
+                        )
+                    
+//                printfn $"Letter check in window: {letterCheckInWindow}"
+                
+                // if letterCheckInWindow is all true then the word matches
+                if List.contains false letterCheckInWindow then Result.Error "Not found " else Result.Ok (rowIndex, windowStartingIndex, Down))
+            
+        printfn "Windows: %A" windows
+        let successfulMatches =
+            windows
+            |> List.filter (fun result ->
+                            match result with
+                            | Ok _ -> true
+                            | Error _ -> false)
+            |> List.tryHead
+
+        if successfulMatches.IsSome then successfulMatches.Value else Result.Error "No matching windows"
+
+        
+
+    firstCoordForRow word firstRow 0
+    
+    // look vertically for slices
+    
+    // see if word fits around what is there
--- src/Generator/UnitTest1.fs
+++ src/Generator/UnitTest1.fs
@@ -3,13 +3,33 @@ module Generator.Tests
 open NUnit.Framework
 open Generator.Generator
 
+open Domain.Grid
+
 
 [<SetUp>]
 let Setup () =
     ()
 
 [<Test>]
-let Test1 () =
-    let res = generatePuzzle 2
+let TestMakeEmptyGrid () =
+   
+    Assert.AreEqual((makeEmptyGrid 1), [ [ Cell.Black ] ])
+    Assert.AreEqual((makeEmptyGrid 2), [ [ Cell.Black; Cell.Black ]
+                                         [ Cell.Black; Cell.Black ]; ])
+    Assert.AreEqual((makeEmptyGrid 3), [ [ Cell.Black; Cell.Black; Cell.Black ];
+                                         [ Cell.Black; Cell.Black ; Cell.Black ]
+                                         [ Cell.Black; Cell.Black ; Cell.Black ]])
 
-    Assert.AreEqual(res, 1)
+[<Test>]
+let TestFindLocationForOneByOneGrid () =
+    let grid = [ [ Cell.Black;];  ]
+    
+    // a single letter fits
+    match (findLocationsForWord "A" grid) with
+    | Ok x -> Assert.AreEqual(x, (0, 0, Down))
+    | Error x -> Assert.Fail("Should have been able to place single letter")
+
+    // but not two letters
+    match (findLocationsForWord "AB" grid) with
+    | Ok x -> Assert.Fail("Should not have been able to place two letters")
+    | Error x -> Assert.Pass()
