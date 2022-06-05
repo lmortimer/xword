53050924eb80aa5b4b94d5a6250e3cf119775659
Logan Mortimer <logan@isthisit.nz>
2022-06-05 19:37:41 +1000

    Substring generator

--- src/Generator/Assemble.fs
+++ src/Generator/Assemble.fs
@@ -4,9 +4,19 @@ open Domain.Grid
 open Generator.Generator
 open Generator.Verifier
 
-//  ["bear"] -> ["bear"; "bea"; "be"; "b"]
-let generateWordListWithSubstrings (wordList: string list): string list =
-    ["fo"]
+        
+// when generating the grid we need to OK partially completed words        
+//  ["bear"] -> ["b"; "be"; "bea"; "bear"]
+let generateWordListWithPrefixStrings (wordList: string list): string list =
+    
+    let wordToPrefixes (word: string) =
+        seq {
+            for i in 0..( word.Length - 1) -> word.Substring(0, i + 1)
+        } |> Seq.toList
+    
+    wordList
+    |> List.map wordToPrefixes
+    |> List.concat
 
 let generateGrid (size: int) (wordList: string list) =
     
--- src/Generator/AssembleTests.fs
+++ src/Generator/AssembleTests.fs
@@ -7,6 +7,21 @@ open Grid
 open Generator.Assemble
 
 [<Test>]
+let TestGenerateWordListWithSubstrings () =
+    
+    let emptyWordOutput = generateWordListWithPrefixStrings [""]
+    Assert.IsEmpty(emptyWordOutput)
+    
+    let singleLetterOutput = generateWordListWithPrefixStrings ["A"]
+    Assert.AreEqual(["A"], singleLetterOutput)
+    
+    let multiLetterOutput = generateWordListWithPrefixStrings ["bear"]    
+    Assert.AreEqual(["b"; "be"; "bea"; "bear"], multiLetterOutput)
+
+    let multipleWordsOutput = generateWordListWithPrefixStrings ["hi"; "yo"]
+    Assert.AreEqual(["h"; "hi"; "y"; "yo"], multipleWordsOutput)
+    
+[<Test>]
 let TestAssembleWordsWithExactMatch () =
             
     let wordList = ["aa"; "ab"; "ac"; "a"]
